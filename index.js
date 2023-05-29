daed7635c6c99607b75453808e5bc310f48899fb
jessa602 <jessa602@gmail.com>
2023-05-29 01:53:26 -0700

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+// Prompt 1:
+// Clean the coffee_data file:
+
+// Checklist:
+// a. All prices should be numbers.
+// b. All items should be strings.
+// c. Objects and properties should have commas seperating them.
+// d. Connect the coffee_data.js file to the index.js file.
